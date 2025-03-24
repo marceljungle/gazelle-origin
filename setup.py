bfe8f3009049162470a9b6d1c3697f2fc8956bc8
Gigi Marcel Dan <gigimd@ext.zara.com>
2025-03-24 18:30:41 +0100

    bump version to 3.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gazelle-origin",
-    version="2.2.1",
+    version="3.0.1",
     author="x1ppy",
     author_email="",
     packages=[
